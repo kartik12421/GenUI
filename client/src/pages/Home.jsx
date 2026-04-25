054c025b9b625420d8bd2af15ffd0e996aedc7d8
Kartik Joshi <gopaljoshi8914@gmail.com>
2026-04-26 00:19:56 +0530

    eight commit: client side started to setup. Vite app created and tailwind css also. Routing worki is also started.

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Auth from '../components/Auth.jsx'
+
+function Home() {
+  return (
+    <div className='text-9xl text-[red]'>
+      i am home
+
+      <Auth></Auth>
+    </div>
+  )
+}
+
+export default Home
